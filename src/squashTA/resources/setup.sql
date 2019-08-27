c0b306a31db8132ba04025b6a5ebfc9e4eda220d
EricMayeux <mayeux.er@gmail.com>
2019-08-27 20:22:31 +0200

    eeeeee

--- src/squashTA/resources/setup.sql
+++ src/squashTA/resources/setup.sql
@@ -1,9 +1,9 @@
-delete from logicaldoc.ld_bookmark;
-commit;
-delete from logicaldoc.ld_version;
-commit;
-update logicaldoc.ld_document set ld_deleted = 0, ld_title = 'title_origine' where ld_id = 3571717;
-delete from logicaldoc.ld_document where ld_id != 3571717 AND ld_id != 3964928;
-commit;
-delete from logicaldoc.ld_folder where ld_creator is not null and ld_id != 3670017;
-commit;
+delete from logicaldoc.ld_bookmark;@@
+commit;@@
+delete from logicaldoc.ld_version;@@
+commit;@@
+update logicaldoc.ld_document set ld_deleted = 0, ld_title = 'title_origine' where ld_id = 3571717;@@
+delete from logicaldoc.ld_document where ld_id != 3571717 AND ld_id != 3964928;@@
+commit;@@
+delete from logicaldoc.ld_folder where ld_creator is not null and ld_id != 3670017;@@
+commit;@@
